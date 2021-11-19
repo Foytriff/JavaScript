0b6622e1a2bb04f2a71f5e6f561fa4bacb6cadd0
foytriff <gustavhf73@gmail.com>
2021-11-19 15:10:09 +0100

    Redo 1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+/*
 //Variables and Elements
 let deck = {};
 let previousCard = {};
@@ -95,3 +96,47 @@ async function lowDraw() {
     }
     previousCard = nextCard;
 }
+*/
+let deck = {};
+async function getDeck() {
+    var startTime = performance.now()
+    let res = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+    let data = await res.json();
+    deck = data;
+    var endTime = performance.now()
+    console.log(`Call to doSomething took ${endTime - startTime} milliseconds`)
+}
+getDeck();
+
+//Create card elements and Appending
+let currentCardImg = document.createElement("img");
+let prevCardImg = document.createElement("img");
+const faceCardCont = document.querySelector(".faceCardsContainer");
+const prevCard = faceCardCont.querySelector(".prevCard");
+const currentCard = faceCardCont.querySelector(".currentCard");
+prevCard.append(prevCardImg);
+currentCard.append(currentCardImg);
+
+// Create Button and Listener
+let button = document.createElement("button");
+document.body.append(button);
+button.addEventListener("click", drawCard);
+button.innerText = "draw";
+
+//Card Draw Functions
+function setPrevCardToCurrentCard() {
+    console.log(currentCardImg.getAttribute("src"));
+    prevCardImg.setAttribute = ("src", currentCardImg.getAttribute("src"));
+}
+
+function drawCard() {
+    setTimeout(async () => {
+        let res = fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`)
+        let data = await (await res).json();
+        currentCardImg.setAttribute("src", `${data.cards[0].image}`);
+        setPrevCardToCurrentCard();
+    }, 1000);
+}
+
+
+
